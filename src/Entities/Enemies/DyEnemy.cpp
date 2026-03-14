27829210db7aaf5c13eacaf2707083cfff6582d7
ianrivera <ian.rivera28@upr.edu>
2026-03-13 23:11:27 -0400

    bug fixes

--- src/Entities/Enemies/DyEnemy.cpp
+++ src/Entities/Enemies/DyEnemy.cpp
@@ -4,23 +4,16 @@
 void DyEnemy::draw() {
     Rectangle source;
     if (HitBox::drawHitbox) this->hitBox.draw();
-    DrawTexturePro(ImageManager::SpriteSheet, Rectangle{2, 128, 13, 14}, 
+        texture = GetRandomValue(0, 1); 
+        if (texture == 0) {
+            source = Rectangle{2, 128, 13, 14};
+        } else {
+            source = Rectangle{17, 128, 13, 14};
+        }
+       DrawTexturePro(ImageManager::SpriteSheet, source, 
                 Rectangle{this->position.first + 15, this->position.second + 15, 30, 30}, 
                 Vector2{15, 15}, this->aimAngle, WHITE);
-
-    if (textureType == 0) {
-        sources = { Rectangle{2, 128, 13, 14}
-    }
-    else {
-        sources = Rectangle{2, 147, 13, 13},;
-    }
-    }
 }
-    DrawTexturePro(ImageManager::SpriteSheet, source,
-                Rectangle{this->position.first + 15, this->position.second + 15, 30, 30}, 
-                Vector2{15, 15}, this->aimAngle, WHITE);
-
-
 void DyEnemy::update(std::pair<float, float> pos, HitBox target) {
     this->cooldown--;
 
